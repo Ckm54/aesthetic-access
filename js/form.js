363b139e75e25b97abc14649b6ec6b9877ba3a40
Collo <mutaic84@gmail.com>
2022-03-23 17:54:02 +0300

    update name and onclick methods

---
+++ js/form.js
@@ -0,0 +1,11 @@
+
+function register() {
+    $("#login").css("left", "-400px");
+    $("#register").css("left", "50px");
+    $("#btn").css("left", "110px");
+};
+function login() {
+    $("#login").css("left", "50px");
+    $("#register").css("left", "450px");
+    $("#btn").css("left", "0px");
+};
