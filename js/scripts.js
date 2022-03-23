5d74394d03925b05c8092d2a95fd4706aa422c39
FloKots <florencekotohoyoh@gmail.com>
2022-03-23 04:46:24 +0300

    add dummy form

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+
+//Get height and width of wall 1. If time allows, have a function to select the units, but currently in metres.
+// Have a button to add more walls
+//Calculate area of each wall.
+//Get total area of walls
+//Input the height and width for the other wall
+//Get the height and width of window 1
+// Have a button to add more windows
+//Calculate area of each window
+//Get the total area of windows
+//Get height and width of door
+//Have a button to add other doors
+//Calculate area of  each door.
+//Get total area of windows
+//Check if there are any additional spaces to be excluded
+//Get height and width of the additional spaces
+//Calculate area of the additional spaces
+//Get total area of additional spaces
+//Include 10% wastage
+//Calculate total full coverage area  i.e walls + 10 % wastage
+//Calculate total area to be excluded i.e windows, doors, additional spaces
+//Calculate area to be covered by wallpaper i.e full coverage area - wallpaper area
+//Calculate wallpaper area
+//Calculate number of wallpaper rolls i.e total area/wallpaper area
+//Display area and number of wallpaper rolls
