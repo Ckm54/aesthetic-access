0e76dea83172efff136947afe6f4a3291137e94c
FloKots <florencekotohoyoh@gmail.com>
2022-03-23 07:30:10 +0300

    add wall constructor, area prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,41 @@
+$(document).ready(function () {
+  $("#wallPaperCalculator").submit(function (event) {
+    function calculateArea(width, height) {
+      var calculatedArea = width * height;
+      console.log(`I have been called at the function level. The Area is: ${calculatedArea}`)
+    }
+    //TODO: Create Wall Constructor
+    function Wall(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+     //TODO: Add Area Prototype to Wall
+     Wall.prototype.area = calculateArea;
+    
+   
+    // * Get Wall Width
+    var wall1Width = $("#wall1Width").val();
+    //* Get Wall Height
+    var wall1Height = $("#wall1Height").val();
+    
+    //* Declare wall 1
+    var wall1 = new Wall(wall1Width, wall1Height);
+    console.log("I am wall1: " + "Width: " + wall1.width, "Height: " + wall1.height);
+    //* Calculate the area of wall 1
+    wall1.area(wall1.width, wall1.height);
+    console.log("You called me!!")
 
+    // calculateArea(wall1.width, wall1.height);
+    // "My Area is: " + wall1.area(wall1.width, wall1.height)
+    // wall1.area();
+    // console.log("The wall area prototype calculated me. Area: " + wall1.area)
+    //TODO: add wallWidth class to wall inputs
+
+    //TODO: Uncomment the line below
+    //* this.reset();
+    event.preventDefault();
+  })
+})
 //Get height and width of wall 1. If time allows, have a function to select the units, but currently in metres.
 // Have a button to add more walls
 //Calculate area of each wall.
